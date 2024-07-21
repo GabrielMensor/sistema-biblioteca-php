1afbb337779e80971e8fde4e9b78a597bcca6253
Gabriel Mensor <gabrielmensor@hotmail.com>
2024-07-21 00:49:11 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Página inicial da Small Library, o gerenciador da biblioteca" />  <!-- o texto de descrição que aparece em letras pequenas, embaixo do título nos buscadores -->
+    <title>Documento</title>
+    <link rel="stylesheet" href="./assets/css/styles.css">
+</head>
+<body>
+    <header>
+        <h1><a href="#" id="logo" class="clickable-normal-text">SMALL LIBRARY</a></h1>
+        <nav>
+            <ul>
+                <li><a href="#" class="clickable-normal-text normal-li">Livros</a></li>
+                <li><a href="#" class="clickable-normal-text normal-li">Sobre</a></li>
+                <li><a href="#" class="clickable-normal-text" id="login">Fazer Login</a></li>
+            </ul>
+        </nav>
+    </header>
+<!--     <main>
+
+    </main>
+    <footer>
+
+    </footer> -->
+</body>
+</html>
