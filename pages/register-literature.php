711fe9e2c9c220aae3c28478c109519b2400e4b0
Gabriel Mensor <gabrielmensor@hotmail.com>
2024-12-29 20:40:18 -0300

    diversas melhorias e avanços nos selects do form de cadastro de obras

--- pages/register-literature.php
+++ pages/register-literature.php
@@ -15,6 +15,24 @@ $resultado_publisher = $mysqli->query($sql_publisher);
 
 $sql_author = "select id, name from author";
 $resultado_author = $mysqli->query($sql_author);
+
+$sql_location = "select id, name from location";
+$resultado_location = $mysqli->query($sql_location);
+
+$sql_literature_type = "select id, name from literature_type";
+$resultado_literature_type = $mysqli->query($sql_literature_type);
+
+$sql_languages = "select id, name from languages";
+$resultado_languages = $mysqli->query($sql_languages);
+
+$sql_availability = "select id, state from availability";
+$resultado_availability = $mysqli->query($sql_availability);
+
+$sql_origin = "select id, name from origin";
+$resultado_origin = $mysqli->query($sql_origin);
+
+$sql_format = "select id, name from format";
+$resultado_format = $mysqli->query($sql_format);
 ?>
 
 <!DOCTYPE html>
@@ -38,65 +56,95 @@ $resultado_author = $mysqli->query($sql_author);
                 <div id="form-column-left">
                     <select name="type" id="type-select">
                         <option value="">Tipo</option>
-                        <option value=""></option>
-                        <option value=""></option>
-                        <option value=""></option>
-                        <option value=""></option>
+                        <?php while($linha = $resultado_literature_type->fetch_assoc()): ?>
+                            <option value="<?= $linha['id'] ?>">
+                                <?= htmlentities($linha['name'], ENT_QUOTES, "UTF-8"); ?>
+                            </option>
+                        <?php endwhile ?>
                     </select>
                     <input type="text" id="isbn" placeholder="ISBN">
                     <input type="text" id="title" placeholder="Título">
                     <select name="literature_language" id="literature_language_select">
-                        <option value="">Idioma</option>
-                        <option value=""></option>
-                        <option value=""></option>
-                        <option value=""></option>
-                        <option value=""></option>
+                        <option value="">Idioma 1</option>
+                        <?php while($linha = $resultado_languages->fetch_assoc()): ?>
+                            <option value="<?= $linha['id'] ?>">
+                                <?= htmlentities($linha['name'], ENT_QUOTES, "UTF-8"); ?>
+                            </option>
+                        <?php
+                            endwhile;
+                            $resultado_languages->data_seek(0); // Resetando o ponteiro do while
+                        ?>
+                    </select>
+                    <select name="literature_language" id="literature_language_select">
+                        <option value="">Idioma 2</option>
+                        <?php while($linha = $resultado_languages->fetch_assoc()): ?>
+                            <option value="<?= $linha['id'] ?>">
+                                <?= htmlentities($linha['name'], ENT_QUOTES, "UTF-8"); ?>
+                            </option>
+                        <?php
+                            endwhile;
+                            $resultado_languages->data_seek(0); // Resetando o ponteiro do while
+                        ?>
+                    </select>
+                    <select name="literature_language" id="literature_language_select">
+                        <option value="">Idioma 3</option>
+                        <?php while($linha = $resultado_languages->fetch_assoc()): ?>
+                            <option value="<?= $linha['id'] ?>">
+                                <?= htmlentities($linha['name'], ENT_QUOTES, "UTF-8"); ?>
+                            </option>
+                        <?php endwhile ?>
                     </select>
                     <input type="text" id="publication_date_select" placeholder="Ano de publicação">
                     <textarea name="summary" id="summary" rows="5" cols="33" placeholder="Resumo"></textarea>
                     <input type="text" name="pages" id="pages" placeholder="Nº Páginas">
                     <input type="file" name="cover-image" id="cover-image">
-                    <input type="text" name="edition" id="edition" placeholder="Edição">
                 </div>
                 <div id="form-column-right">
+                    <input type="text" name="edition" id="edition" placeholder="Edição">
                     <select name="format" id="format">
                         <option value="">Formato</option>
-                        <option value="ebook">E-book</option>
-                        <option value="fisico">Físico</option>
+                        <?php while($linha = $resultado_format->fetch_assoc()): ?>
+                            <option value="<?= $linha['id'] ?>">
+                                <?= htmlentities($linha['name'], ENT_QUOTES, "UTF-8"); ?>
+                            </option>
+                        <?php endwhile ?>
                     </select>
                     <input type="text" name="dimensions" id="dimensions" placeholder="Dimensões (modelo 29x21)">
                     <input type="text" name="keywords" id="keywords" placeholder="Palavras-chave (separe por vírgulas)">
                     <select name="availability" id="availability">
                         <option value="">Disponibilidade</option>
-                        <option value="arquivado">Arquivado</option>
-                        <option value="vindoemprestimo">Vindo de empréstimo</option>
-                        <option value="emprestadoalguem">Emprestado a alguém</option>
-                        <option value="dado">Dado/vendido a algo/alguém</option>
+                        <?php while($linha = $resultado_availability->fetch_assoc()): ?>
+                            <option value="<?= $linha['id'] ?>">
+                                <?= htmlentities($linha['state'], ENT_QUOTES, "UTF-8"); ?>
+                            </option>
+                        <?php endwhile ?>
                     </select>
                     <select name="origin" id="origin">
                         <option value="">Origem</option>
-                        <option value=""></option>
-                        <option value=""></option>
-                        <option value=""></option>
-                        <option value=""></option>
+                        <?php while($linha = $resultado_origin->fetch_assoc()): ?>
+                            <option value="<?= $linha['id'] ?>">
+                                <?= htmlentities($linha['name'], ENT_QUOTES, "UTF-8"); ?>
+                            </option>
+                        <?php endwhile ?>
                     </select>
                     <select name="location" id="location">
                         <option value="">Localização</option>
-                        <option value=""></option>
-                        <option value=""></option>
-                        <option value=""></option>
-                        <option value=""></option>
+                        <?php while($linha = $resultado_location->fetch_assoc()): ?>
+                            <option value="<?= $linha['id'] ?>">
+                                <?= htmlentities($linha['name'], ENT_QUOTES, "UTF-8"); ?>
+                            </option>
+                            <?php endwhile ?>
                     </select>
                     <select name="author" id="author">
-                        <option value="">Selecione o autor</option>
+                        <option value="">Autor</option>
                         <?php while($linha = $resultado_author->fetch_assoc()): ?>
                             <option value="<?= $linha['id']; ?>">
                                 <?= htmlentities($linha['name'], ENT_QUOTES, 'UTF-8'); ?>
-                            <option>
+                            </option>
                             <?php endwhile ?>
                     </select>
                     <select name="publisher" id="publisher">
-                        <option value="">Selecione a editora</option>
+                        <option value="">Editora</option>
                         <?php while($linha = $resultado_publisher->fetch_assoc()): ?>
                             <option value="<?= $linha['id']; ?>">
                                 <?= htmlentities($linha['name'], ENT_QUOTES, 'UTF-8'); ?>
@@ -104,7 +152,7 @@ $resultado_author = $mysqli->query($sql_author);
                             <?php endwhile ?>
                     </select>
                     <select name="category" id="category">
-                        <option value="">Selecione a categoria</option>
+                        <option value="">Categoria</option>
                         <?php while($linha = $resultado_category->fetch_assoc()): ?>
                             <option value="<?= $linha['id']; ?>">
                                 <?= htmlentities($linha['name'], ENT_QUOTES, 'UTF-8'); ?>
